836915f7daf3b697bb0be8c47498560b88f38a79
Michael.Patel <michael.patel@betgenius.com>
2021-06-27 08:39:55 -0700

    Tidy imports

--- src/main/kotlin/io/github/patelm5/processorsandbox/ProcessorSandboxApplication.kt
+++ src/main/kotlin/io/github/patelm5/processorsandbox/ProcessorSandboxApplication.kt
@@ -1,21 +1,12 @@
 package io.github.patelm5.processorsandbox
 
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import org.apache.kafka.common.serialization.IntegerDeserializer
 import org.apache.kafka.common.serialization.Serdes
 import org.apache.kafka.streams.kstream.Grouped
 import org.apache.kafka.streams.kstream.KStream
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties
-import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.kafka.core.ConsumerFactory
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory
-import org.springframework.kafka.support.DefaultKafkaHeaderMapper
-import org.springframework.kafka.support.KafkaHeaderMapper
-import org.springframework.kafka.support.serializer.JsonDeserializer
 import org.springframework.kafka.support.serializer.JsonSerde
 import java.util.function.Function
 
